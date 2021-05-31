b4cdf79dcbb3baf4faaead73c7d8a0c85597fd12
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2021-05-31 18:17:28 -0400

    finished djiksitar

--- 12 Graphs/06-07 Dijkstra's Algorithm/AdjListWeighted.java
+++ 12 Graphs/06-07 Dijkstra's Algorithm/AdjListWeighted.java
@@ -1,6 +1,6 @@
 // Name:   
 // Date:
- 
+
 import java.util.*;
 import java.io.*;
 
@@ -9,115 +9,147 @@ import java.io.*;
  *              Graphs7: Dijkstra with Cities
  */
 
-class Edge 
-{
-   public final wVertex target;  //if it's public, you don't need accessor methods
-   public final double weight;   //if it's public, you don't need accessor methods
-  
-   public Edge(wVertex argTarget, double argWeight) 
-   {
+class Edge {
+   public final wVertex target; // if it's public, you don't need accessor methods
+   public final double weight; // if it's public, you don't need accessor methods
+
+   public Edge(wVertex argTarget, double argWeight) {
       target = argTarget;
       weight = argWeight;
    }
 }
 
-interface wVertexInterface 
-{
+interface wVertexInterface {
    String getName();
+
    double getMinDistance();
+
    void setMinDistance(double m);
-   //wVertex getPrevious();   //for Dijkstra 7
-   //void setPrevious(wVertex v);  //for Dijkstra 7
+
+   // wVertex getPrevious(); //for Dijkstra 7
+   // void setPrevious(wVertex v); //for Dijkstra 7
    ArrayList<Edge> getAdjacencies();
-   void addEdge(wVertex v, double weight);   
+
+   void addEdge(wVertex v, double weight);
+
    int compareTo(wVertex other);
 }
 
-class wVertex implements Comparable<wVertex>, wVertexInterface
-{
+class wVertex implements Comparable<wVertex>, wVertexInterface {
    private final String name;
    private ArrayList<Edge> adjacencies;
    private double minDistance = Double.POSITIVE_INFINITY;
-   //private wVertex previous;  //for building the actual path in Dijkstra 7
-   
-   /*  enter your code for this class here   */ 
-   public wVertex (String argName) {
+   // private wVertex previous; //for building the actual path in Dijkstra 7
+
+   /* enter your code for this class here */
+   public wVertex(String argName) {
       this.name = argName;
       adjacencies = new ArrayList<Edge>();
    }
+
    public String getName() {
       return name;
    }
+
    public double getMinDistance() {
       return minDistance;
    }
+
    public void setMinDistance(double newMinDistance) {
       minDistance = newMinDistance;
    }
+
    public ArrayList<Edge> getAdjacencies() {
       return adjacencies;
    }
+
    public void addEdge(wVertex v, double weight) {
       adjacencies.add(new Edge(v, weight));
    }
+
    public int compareTo(wVertex other) {
       return (int) (minDistance - other.minDistance);
    }
 }
 
-interface AdjListWeightedInterface 
-{
+interface AdjListWeightedInterface {
    List<wVertex> getVertices();
+
    Map<String, Integer> getNameToIndex();
-   wVertex getVertex(int i);   
+
+   wVertex getVertex(int i);
+
    wVertex getVertex(String vertexName);
+
    void addVertex(String v);
+
    void addEdge(String source, String target, double weight);
-   void minimumWeightPath(String vertexName);   //Dijkstra's
+
+   void minimumWeightPath(String vertexName); // Dijkstra's
 }
 
-/* Interface for Graphs 7:  Dijkstra with Cities 
+/*
+ * Interface for Graphs 7: Dijkstra with Cities
  */
 
-interface AdjListWeightedInterfaceWithCities 
-{       
+interface AdjListWeightedInterfaceWithCities {
    List<String> getShortestPathTo(wVertex v);
-   AdjListWeighted graphFromEdgeListData(File vertexNames, File edgeListData) throws FileNotFoundException ;
-}
- 
 
-public class AdjListWeighted implements AdjListWeightedInterface //,AdjListWeightedInterfaceWithCities
+   AdjListWeighted graphFromEdgeListData(File vertexNames, File edgeListData) throws FileNotFoundException;
+}
+
+public class AdjListWeighted implements AdjListWeightedInterface // ,AdjListWeightedInterfaceWithCities
 {
    private List<wVertex> vertices = new ArrayList<wVertex>();
    private Map<String, Integer> nameToIndex = new HashMap<String, Integer>();
-   //the constructor is a no-arg constructor 
-  
-   /*  enter your code for Graphs 6 */ 
+   // the constructor is a no-arg constructor
+
+   /* enter your code for Graphs 6 */
    public List<wVertex> getVertices() {
       return vertices;
    }
+
    public wVertex getVertex(int i) {
       return vertices.get(i);
    }
+
    public Map<String, Integer> getNameToIndex() {
       return nameToIndex;
    }
+
    public wVertex getVertex(String vertexName) {
       return vertices.get(nameToIndex.get(vertexName));
    }
+
    public void addVertex(String v) {
       vertices.add(new wVertex(v));
+      nameToIndex.put(v, vertices.size() - 1);
    }
+
    public void addEdge(String source, String target, double weight) {
-      // boolean exists = false;
-      // for (wVertex v : vertices) {
-      //    if (v.getName().equals(target))
-      // }
+      boolean sourceExists = false;
+      boolean targetExists = false;
+      for (wVertex v : vertices) {
+         if (v.getName().equals(source)) {
+            sourceExists = true;
+         }
+         if (v.getName().equals(target)) {
+            targetExists = true;
+         }
+      }
+      if (!sourceExists) {
+         addVertex(source);
+      }
+      if (!targetExists) {
+         addVertex(target);
+      }
       vertices.get(nameToIndex.get(source)).addEdge(vertices.get(nameToIndex.get(target)), weight);
    }
+
    public void minimumWeightPath(String vertexName) {
       minimumWeightPath(vertices.get(nameToIndex.get(vertexName)));
    }
+
    public void minimumWeightPath(wVertex source) {
       source.setMinDistance(0);
       PriorityQueue<wVertex> pq = new PriorityQueue<>();
@@ -133,9 +165,6 @@ public class AdjListWeighted implements AdjListWeightedInterface //,AdjListWeigh
          }
       }
    }
-   /*  enter your code for two new methods in Graphs 7 */
-   
-   
-}   
+   /* enter your code for two new methods in Graphs 7 */
 
-
+}
